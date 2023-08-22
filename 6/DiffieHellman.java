057cef5f5de787f7c1b375d29c740c2d3e732fea
Aravinda Kumar <76619616+surprisedPikachu007@users.noreply.github.com>
2023-08-22 16:48:58 +0000

    diffie hellman

--- 6/DiffieHellman.java
+++ 6/DiffieHellman.java
@@ -1,16 +1,15 @@
 class DiffieHellman {
     public static void main(String args[]) {
-        int p = 23; /* publicly known (prime number) */
-        int g = 5; /* publicly known (primitive root) */
-        int x = 4; /* only Alice knows this secret */
-        int y = 3; /* only Bob knows this secret */
+        int p = 51; /* publicly known (prime number) */
+        int g = 6; /* publicly known (primitive root) */
+        int x = 5; /* only Alice knows this secret */
+        int y = 2; /* only Bob knows this secret */
         double aliceSends = (Math.pow(g, x)) % p;
         double bobComputes = (Math.pow(aliceSends, y)) % p;
         double bobSends = (Math.pow(g, y)) % p;
         double aliceComputes = (Math.pow(bobSends, x)) % p;
         double sharedSecret = (Math.pow(g, (x * y))) % p;
-        System.out.println(
-                "simulation of Diffie-Hellman key exchange algorithm\n---------------------------------------------");
+        System.out.println("Simulation of Diffie-Hellman key exchange algorithm\n");
         System.out.println("Alice Sends : " + aliceSends);
         System.out.println("Bob Computes : " + bobComputes);
         System.out.println("Bob Sends : " + bobSends);
@@ -18,7 +17,7 @@ class DiffieHellman {
         System.out.println("Shared Secret : " + sharedSecret);
         /* shared secrets should match and equality is transitive */
         if ((aliceComputes == sharedSecret) && (aliceComputes == bobComputes))
-            System.out.println("Success: Shared Secrets Matches! " + sharedSecret);
+            System.out.println("Success: \nShared Secrets Matches! \nSecret: " + sharedSecret);
         else
             System.out.println("Error: Shared Secrets does not Match");
     }
